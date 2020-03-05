21aee39b55082ca9b323abd96a47afad813f65f3
Mina Malaj <mina.malaj@hotmail.com>
2020-03-05 01:18:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,8 @@ def my_hash
 end 
 
 
-def pioneer  = { 
-  :name => 'Grace Hopper'
-} 
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+def pioneer 
+  other hash = {:name => 'Grace Hopper'} 
 end
 
 def id_generator
