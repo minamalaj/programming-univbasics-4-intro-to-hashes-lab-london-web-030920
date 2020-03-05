715074199eb0266467f3d2512e4325cb5bb8b95b
Mina Malaj <mina.malaj@hotmail.com>
2020-03-05 01:22:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
